b6205eaf0c63239d26f572a0318de3a74599dd11
Alexander Kravchenko <alexandercray05@gmail.com>
2025-04-14 23:08:06 +0300

    added repo layer

---
+++ internal/repository/postgres/models/pgmodels.go
@@ -0,0 +1,58 @@
+package models
+
+import "time"
+
+// everywhere is SQLx style db tags
+
+type UserPosition struct {
+	ID   int    `db:"id"`
+	Name string `db:"name"`
+}
+
+type User struct {
+	UID        string     `db:"uid"`
+	Name       string     `db:"name"`
+	Surname    string     `db:"surname"`
+	PositionID int        `db:"position_id"`
+	Email      string     `db:"email"`
+	Phone      string     `db:"phone"`
+	HireDate   *time.Time `db:"hire_date"`
+	LastOnline *time.Time `db:"last_online"`
+	PfpURL     *string    `db:"pfp_url"`
+	IsAdmin    bool       `db:"is_admin"`
+}
+
+type IdeaCategory struct {
+	ID   int    `db:"id"`
+	Name string `db:"name"`
+}
+
+type IdeaStatus struct {
+	ID   int    `db:"id"`
+	Name string `db:"name"`
+}
+
+type Idea struct {
+	IdeaUID      string    `db:"idea_uid"`
+	Author       string    `db:"author"`
+	CreationDate time.Time `db:"creation_date"`
+	StatusID     int       `db:"status_id"`
+	CategoryID   int       `db:"category_id"`
+	LikeCount    int       `db:"like_count"`
+	DislikeCount int       `db:"dislike_count"`
+}
+
+type Comment struct {
+	CommentID   int       `db:"comment_id"`
+	IdeaUID     string    `db:"idea_uid"`
+	AuthorID    string    `db:"author_id"`
+	Timestamp   time.Time `db:"timestamp"`
+	CommentText string    `db:"comment_text"`
+}
+
+type Reply struct {
+	CommentID int       `db:"comment_id"`
+	AuthorID  string    `db:"author_id"`
+	Timestamp time.Time `db:"timestamp"`
+	ReplyText string    `db:"reply_text"`
+}
--- internal/repository/postgres/postgres.go
+++ internal/repository/postgres/postgres.go
@@ -1,56 +1,20 @@
-package pgmodels
+package postgres
 
-import "time"
+import (
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
 
-type UserPosition struct {
-	ID   int    `db:"id"` // SQLx style tag
-	Name string `db:"name"`
+type Postgres struct {
+	db *sqlx.DB
 }
 
-type User struct {
-	UID        string     `db:"uid"` // UUID
-	Name       string     `db:"name"`
-	Surname    string     `db:"surname"`
-	PositionID int        `db:"position_id"`
-	Email      string     `db:"email"`
-	Phone      string     `db:"phone"`
-	HireDate   *time.Time `db:"hire_date"`   // DATE
-	LastOnline *time.Time `db:"last_online"` // TIMESTAMP
-	PfpURL     *string    `db:"pfp_url"`     // TEXT
-	IsAdmin    bool       `db:"is_admin"`    // BOOL
-}
+func NewPostgres(sourceName string) (*Postgres, error) {
+	db, err := sqlx.Connect("postgres", sourceName)
 
-type IdeaCategory struct {
-	ID   int    `db:"id"`
-	Name string `db:"name"`
-}
+	if err != nil {
+		return nil, err
+	}
+	return &Postgres{db: db}, nil
 
-type IdeaStatus struct {
-	ID   int    `db:"id"`
-	Name string `db:"name"`
-}
-
-type Idea struct {
-	IdeaUID      string    `db:"idea_uid"`      // UUID
-	Author       string    `db:"author"`        // UUID
-	CreationDate time.Time `db:"creation_date"` // consider using time.Time with TIMESTAMP
-	StatusID     int       `db:"status_id"`
-	CategoryID   int       `db:"category_id"`
-	LikeCount    int       `db:"like_count"`
-	DislikeCount int       `db:"dislike_count"`
-}
-
-type Comment struct {
-	CommentID   int       `db:"comment_id"`
-	IdeaUID     string    `db:"idea_uid"`     // UUID
-	AuthorID    string    `db:"author_id"`    // UUID
-	Timestamp   time.Time `db:"timestamp"`    // TIMESTAMP
-	CommentText string    `db:"comment_text"` // TEXT
-}
-
-type Reply struct {
-	CommentID int       `db:"comment_id"`
-	AuthorID  string    `db:"author_id"`  // UUID
-	Timestamp time.Time `db:"timestamp"`  // TIMESTAMP
-	ReplyText string    `db:"reply_text"` // TEXT
 }
