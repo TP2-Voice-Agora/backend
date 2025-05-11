0673fbafe4196cd1ff9b18bb994ba9eddfd81105
Alexander Kravchenko <alexandercray05@gmail.com>
2025-05-11 12:54:45 +0300

    feat: added authRegister

--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -3,7 +3,9 @@ package auth
 import (
 	"context"
 	"errors"
+	"github.com/google/uuid"
 	"gitlab.com/ictisagora/backend/internal/lib/jwt"
+	"gitlab.com/ictisagora/backend/internal/models"
 	"gitlab.com/ictisagora/backend/internal/repository"
 	"golang.org/x/crypto/bcrypt"
 	"log/slog"
@@ -11,17 +13,50 @@ import (
 )
 
 type Auth struct {
-	log      *slog.Logger
-	repo     repository.Repository
-	tokenTTL time.Duration
+	log       *slog.Logger
+	repo      repository.Repository
+	tokenTTL  time.Duration
+	jwtSecret string
 }
 
-func New(log *slog.Logger, repo repository.Repository, tokenTTL time.Duration) *Auth {
+func New(log *slog.Logger, repo repository.Repository, tokenTTL time.Duration, jwtSecret string) *Auth {
 	return &Auth{
-		log:      log,
-		repo:     repo,
-		tokenTTL: tokenTTL,
+		log:       log,
+		repo:      repo,
+		tokenTTL:  tokenTTL,
+		jwtSecret: jwtSecret,
 	}
+}
+
+func (a *Auth) Register(u models.User) error {
+	op := "AuthRegister"
+	log := a.log.With(
+		slog.String("op", op),
+		slog.String("uid", u.UID),
+	)
+
+	log.Info("attempting to register user")
+
+	hashPass, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		log.Error("failed to generate password hash")
+		return err
+	}
+
+	u.Password = string(hashPass)
+
+	uid := uuid.New().String()
+
+	u.UID = uid
+
+	err = a.repo.InsertUser(u)
+	if err != nil {
+		log.Error("failed to insert user")
+		return err
+	}
+	log.Info("successfully registered user")
+
+	return nil
 }
 
 func (a *Auth) Login(
@@ -51,7 +86,7 @@ func (a *Auth) Login(
 
 	log.Info("user logged in")
 
-	token := jwt.NewToken(user, a.tokenTTL, "dasdbasdhas")
+	token := jwt.NewToken(user, a.tokenTTL, a.jwtSecret)
 
 	return token, nil
 }
